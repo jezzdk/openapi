0631f3ba044017ff1c37882c8915958007857210
Jess Stopa <jess@stopa.dk>
2021-09-23 22:43:21 +0200

    Updated config

--- config/config.php
+++ config/config.php
@@ -8,7 +8,8 @@ return [
     'docs_path' => base_path('docs.php'),
 
     /**
-     * The path to the file where the generated documentation will be created (supports both .json and .yaml).
+     * The path to the file where the generated documentation will be stored.
+     * The generator supports both .json and .yaml files.
      */
     'output_path' => base_path('docs.json'),
 
